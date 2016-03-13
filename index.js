78a4f31bcc5d869542483fe641d78fbb57b169f3
Brian Dennis <briandennis321@gmail.com>
2016-03-13 00:26:11 -0600

    added functionality, basic parsing

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ var stream = require('stream');
 var ogFileName = process.argv[2];
 var outFileName = process.argv[3];
 
-var readStream = fs.createReadStream( _dirname + ogFileName);
-var writeStream = fs.createWriteStream(_dirname + outFileName);
+var readStream = fs.createReadStream( __dirname + ogFileName);
+var writeStream = fs.createWriteStream( __dirname + outFileName);
+
+readStream.setEncoding('utf8');
+
+readStream.on('data', function(data){
+  var dataArray = data.split(' ');
+  dataArray.forEach(function(word, index){
+    var matches = ['is', 'IS','Is'];
+    if(matches.indexOf(word.slice(0,2)) >= 0){
+      //update chunk
+    }
+  });
+});
 
 readStream.pipe(writeStream);
---
+++ output/main.js
@@ -0,0 +1,6 @@
+var hello = 'world!';
+var isTrue = true;
+
+if(isTrue){
+  console.log(hello);
+}
