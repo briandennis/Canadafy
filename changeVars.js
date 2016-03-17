23e88826b2af971ba852e589f892cd90d7ff4388
Brian Dennis <briandennis321@gmail.com>
2016-03-17 11:59:12 -0500

    now parses nodes to get variables

--- changeVars.js
+++ changeVars.js
@@ -1,4 +1,10 @@
 module.exports = function(fileInfo, api) {
-  var variables =  api.jscodeshift(fileInfo.source).findVariableDeclarators('foo');
-  console.log(variables);    
+  var variables =  api.jscodeshift(fileInfo.source).findVariableDeclarators();
+  console.log(variables.nodes().reduce(function(prevVal, item){
+    console.log(item.id.type);
+    if(item.id.type === 'Identifier'){
+      prevVal.push(item.id);
+    }
+    return prevVal;
+  }, []).length);
 };
